3b5a1df751280a6de458a71619752790a7e5c566
giscafer <giscafer@outlook.com>
2017-01-18 19:57:31 +0800

    update gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import Perf  from 'react-addons-perf';
-
-import App from './components/Main';
-
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
